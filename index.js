965588998b46d23e5dd6a4fc24ce27278ef5d44e
Cathal Dwyer <cathaldwyer7@outlook.com>
2021-11-22 15:45:44 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let example = ["Peter"]
+
+function likes(names) {
+    if (names.length < 1){
+      return `no one likes this`
+      
+    }
+   }
+   likes([example])
+   
+   //let example1 = ["Peter"]
+   //let example2 = ["Jacob", "Alex"]
+   //let example3 = ["Max", "John", "Mark"]
+   //let example4 = ["Alex", "Jacob", "Mark", "Max"]
